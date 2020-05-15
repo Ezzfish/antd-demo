76cf0c4f5c991a20cc507c36814cfb4a9c80842d
Ezzfish <421929659@qq.com>
2020-05-15 09:50:02 +0800

    Block4片头使用Tab+col完成

--- src/BlockFour.js
+++ src/BlockFour.js
@@ -14,7 +14,6 @@ function addd(i){
 }
 class BlockFour extends React.Component{
     render(){
-
         const value=[70,60,20,30,40,40,50,30,20,20]
         return(
             <div>
@@ -23,7 +22,6 @@ class BlockFour extends React.Component{
                         defaultActiveKey="1"
                         tabPostion='botton'
                         style={{height:'100%'}}
-                      
                     >
                         {[...Array(10).keys()].map(i=>(
                             <TabPane tab={ BlockFourTabChart(value, i) }key={i}>
--- src/BlockFourTabChart.js
+++ src/BlockFourTabChart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from 'antd'
 import {
     Chart,
     Geom,
@@ -23,20 +24,54 @@ function BLockFourTabChart(values,i) {
         { name: '2', value: 100 - values[i] },
     ];
     return (
-        <div style={{ width: 100, height: 60 }}>
-            <Chart
-                data={data}
-                autoFit
-                pure
-            >
-                <Coordinate type="theta" radius={0.65} innerRadius={0.55} />
-                <Geom
-                    type="interval"
-                    position="value"
-                    color="name"
-                    adjust={["stack"]}
-                />
-            </Chart>
+        <div style={{ width: 140, height: 140 }}>
+            <Row padding={[12,0]}>
+                <Col span={16}>
+                    <div 
+                        style={{
+                            marginBottom:16,
+                            color:'#00000065',
+                            fontSize:16
+                        }}
+                    >
+                        Stroes {i}</div>
+                    <div
+                        style={{
+                            
+                            color: '#00000045',
+                            fontSize: 14
+                        }}
+                    >
+                        转化率</div>
+                    <div
+                        style={{
+                            marginRight: 16,
+                            color: '#00000085',
+                            fontSize: 24
+                        }}
+                    >
+                        {values[i]}% 
+                    </div>
+                </Col>
+                <Col span={8}>
+                    <Chart
+                        data={data}
+                        width={60}
+                        height={100}
+                        padding={[36,0,0,0]}
+                        pure
+                    >
+                        <Coordinate type="theta" radius={0.65} innerRadius={0.55} />
+                        <Geom
+                            type="interval"
+                            position="value"
+                            color={['name', ['#1890ff', '#F1F2F5']]}
+                            adjust={["stack"]}
+                        />
+                    </Chart>
+                </Col>
+            </Row>
+           
         </div>
     )
 }
