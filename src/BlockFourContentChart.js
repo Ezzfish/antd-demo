9e397ced94a2f2895e7d879639efd4c210abcbd4
Ezzfish <421929659@qq.com>
2020-05-16 09:05:10 +0800

    BlockFour折线图修改数据结构，重新绘图

--- src/BlockFourContentChart.js
+++ src/BlockFourContentChart.js
@@ -22,124 +22,67 @@ import Grid from "antd/lib/card/Grid";
 
 function BlockFourContentChart() {
     const data = [
-        {
-            time: "13:09",
-            passenge: 45,
-            payment: 86
-        },
-        {
-            time: "13:39",
-            passenge: 68,
-            payment: 32
-        }, {
-            time: "14:09",
-            passenge: 70,
-            payment: 59
-        }, {
-            time: "14:39",
-            passenge: 80,
-            payment: 34
-        }, {
-            time: "15:09",
-            passenge: 66,
-            payment: 17
-        }, {
-            time: "15:39",
-            passenge: 33,
-            payment: 90
-        },
-        {
-            time: "16:09",
-            passenge: 70,
-            payment: 78
-        },
-        {
-            time: "16:39",
-            passenge: 54,
-            payment: 70
-        },
-        {
-            time: "17:09",
-            passenge: 28,
-            payment: 56
-        },
-        {
-            time: "17:39",
-            passenge: 37,
-            payment: 19
-        },
-        {
-            time: "18:09",
-            passenge: 95,
-            payment: 83
-        },
-        {
-            time: "18:39",
-            passenge: 58,
-            payment: 53
-        },
-        {
-            time: "19:09",
-            passenge: 83,
-            payment: 64
-        },
-        {
-            time: "19:39",
-            passenge: 21,
-            payment: 79
-        },
-        {
-            time: "20:09",
-            passenge: 70,
-            payment: 102
-        },
-        {
-            time: "20:39",
-            passenge: 51,
-            payment: 75
-        },
-        {
-            time: "21:09",
-            passenge: 21,
-            payment: 103
-        },
-        {
-            time: "21:39",
-            passenge: 97,
-            payment: 75
-        },
-        {
-            time: "22:09",
-            passenge: 48,
-            payment: 87
-        },
+        { name: 'passenge', time: "13:09", value: 45, },
+        { name: 'passenge', time: "13:39", value: 68, },
+        { name: 'passenge', time: "14:09", value: 70, },
+        { name: 'passenge', time: "14:39", value: 80, },
+        { name: 'passenge', time: "15:09", value: 66, },
+        { name: 'passenge', time: "15:39", value: 33, },
+        { name: 'passenge', time: "16:09", value: 70, },
+        { name: 'passenge', time: "16:39", value: 54, },
+        { name: 'passenge', time: "17:09", value: 28, },
+        { name: 'passenge', time: "17:39", value: 37, },
+        { name: 'passenge', time: "18:09", value: 95, },
+        { name: 'passenge', time: "18:39", value: 58, },
+        { name: 'passenge', time: "19:09", value: 83, },
+        { name: 'passenge', time: "19:39", value: 21, },
+        { name: 'passenge', time: "20:09", value: 70, },
+        { name: 'passenge', time: "20:39", value: 51, },
+        { name: 'passenge', time: "21:09", value: 21, },
+        { name: 'passenge', time: "21:39", value: 97, },
+        { name: 'passenge', time: "22:09", value: 48, },
+        { name: 'payment', time: "13:09", value: 86 },
+        { name: 'payment', time: "13:39", value: 32 },
+        { name: 'payment', time: "14:09", value: 59 },
+        { name: 'payment', time: "14:39", value: 34 },
+        { name: 'payment', time: "15:09", value: 17 },
+        { name: 'payment', time: "15:39", value: 90 },
+        { name: 'payment', time: "16:09", value: 78 },
+        { name: 'payment', time: "16:39", value: 70 },
+        { name: 'payment', time: "17:09", value: 56 },
+        { name: 'payment', time: "17:39", value: 19 },
+        { name: 'payment', time: "18:09", value: 83 },
+        { name: 'payment', time: "18:39", value: 53 },
+        { name: 'payment', time: "19:09", value: 64 },
+        { name: 'payment', time: "19:39", value: 79 },
+        { name: 'payment', time: "20:09", value: 102 },
+        { name: 'payment', time: "20:39", value: 75 },
+        { name: 'payment', time: "21:09", value: 103 },
+        { name: 'payment', time: "21:39", value: 75 },
+        { name: 'payment', time: "22:09", value: 87 },
     ];
     const scale = {
-        passenge: {
+        value: {
             ticks: [0, 20, 40, 60, 80, 100, 120],
         },
-        payment: {
-            ticks: [0, 20, 40, 60, 80, 100, 120]
-        },
-        time:{
-            tickCount:9
+        time: {
+            tickCount: 9
         }
     }
-  
+
     return (
         <div>
             <Chart
-                padding={[10, 20, 50, 40]}
+                padding={[30, 20, 50, 40]}
                 autoFit
                 height={400}
                 data={data}
                 scale={scale}
             >
-                <Axis name="passenge"/>
-                <Axis name="payment" visible={false} />
-                <Tooltip shared/>
-                <Line position="time*passenge" color='#60f050'/>
-                <Line position="time*payment" color='#1890ff'/>
+                <Axis name="value" />
+                <Tooltip shared />
+                <Legend name='name' position="top" offsetY={-5} />
+                <Line position="time*value" color={['name', ['#60f050', '#1890ff']]} />
             </Chart>
         </div>
     )
